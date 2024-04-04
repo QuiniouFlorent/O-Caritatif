8d4d5504cb83ae6a2c14748bc017775c384d47d0
unknown <quiniouflorent@gmail.com>
2024-04-04 15:19:35 +0200

    fix homedata datamapper

--- app/datamapper/homedataDatamapper.js
+++ app/datamapper/homedataDatamapper.js
@@ -14,11 +14,35 @@ const homedataDatamapper = {
     async insertHomedata(newHomedata, image) {
 
         const query = `INSERT INTO homedata
-            (association_name, association_logo_url, image_header_content, adress, first_media_link, second_media_link, third_media_link, boutique_is_active, galery_is_active, event_is_active)
+            (association_name, 
+            association_logo_url, 
+            first_home_image_content, 
+            second_home_image_content, 
+            third_home_image_content, 
+            about_summary_content, 
+            adress, 
+            first_media_link, 
+            second_media_link, 
+            third_media_link, 
+            boutique_is_active, 
+            galery_is_active, 
+            event_is_active)
             VALUES
-            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
+            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12; $13)`;
 
-        const values = [newHomedata.association_name, image, newHomedata.image_header_content, newHomedata.adress, newHomedata.first_media_link, newHomedata.second_media_link, newHomedata.third_media_link, newHomedata.boutique_is_active, newHomedata.galery_is_active, newHomedata.event_is_active];
+        const values = [newHomedata.association_name, 
+            image, 
+            newHomedata.first_home_image_content, 
+            newHomedata.second_home_image_content, 
+            newHomedata.third_home_image_content, 
+            newHomedata.about_summary_content,
+            newHomedata.adress, 
+            newHomedata.first_media_link, 
+            newHomedata.second_media_link, 
+            newHomedata.third_media_link, 
+            newHomedata.boutique_is_active, 
+            newHomedata.galery_is_active, 
+            newHomedata.event_is_active];
     
         return datamapperUtil.executeQuery(query, values);
     },
@@ -27,18 +51,32 @@ const homedataDatamapper = {
 
         const query = `UPDATE homedata SET
             association_name = $1,
-            image_header_content = $2,
-            adress = $3,
-            first_media_link = $4,
-            second_media_link = $5,
-            third_media_link = $6,
-            boutique_is_active = $7,
-            galery_is_active = $8, 
-            event_is_active = $9,
+            first_home_image_content = $2, 
+            second_home_image_content = $3, 
+            third_home_image_content = $4, 
+            about_summary_content = $5,
+            adress = $6,
+            first_media_link = $7,
+            second_media_link = $8,
+            third_media_link = $9,
+            boutique_is_active = $10,
+            galery_is_active = $11, 
+            event_is_active = $12,
             updated_at = NOW()
             WHERE id = 1`;
 
-        const values = [homedataModified.association_name, homedataModified.image_header_content, homedataModified.adress, homedataModified.first_media_link, homedataModified.second_media_link, homedataModified.third_media_link, homedataModified.boutique_is_active, homedataModified.galery_is_active, homedataModified.event_is_active];
+        const values = [homedataModified.association_name,
+            homedataModified.first_home_image_content, 
+            homedataModified.second_home_image_content, 
+            homedataModified.third_home_image_content, 
+            homedataModified.image_header_content, 
+            homedataModified.adress, 
+            homedataModified.first_media_link, 
+            homedataModified.second_media_link, 
+            homedataModified.third_media_link, 
+            homedataModified.boutique_is_active, 
+            homedataModified.galery_is_active, 
+            homedataModified.event_is_active];
         
         return datamapperUtil.executeQuery(query, values);
     },
