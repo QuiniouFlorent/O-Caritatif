4f4619595a1e0490a99be13fa5500daf3c7b1be9
unknown <quiniouflorent@gmail.com>
2024-04-04 09:16:32 +0200

    fix API registration

--- app/service/validation/schema.js
+++ app/service/validation/schema.js
@@ -1,1 +1,18 @@
-import Joi from 'joi';
+import vine from '@vinejs/vine';
+
+const commentSchema = vine.object({});
+
+const executivememberSchema = vine.object({});
+
+const homedataSchema = vine.object({});
+
+const newsSchema = vine.object({});
+
+const userSchema = vine.object({});
+
+export {commentSchema,
+        executivememberSchema,
+        homedataSchema,
+        newsSchema,
+        userSchema            
+};
