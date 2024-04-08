ab8480c81297a96ec426cb625f562a67e1fc6d5d
unknown <quiniouflorent@gmail.com>
2024-04-08 14:17:19 +0200

    update opinion controller

--- app/controller/opinionController.js
+++ app/controller/opinionController.js
@@ -34,8 +34,8 @@ const opinionController = {
 
     async updateOpinionPosition( req, res, next ) {
         logger('Opinion modify Position controller called');
-        const opinionModifiedPosition = req.body;
-        const { result , error } = await opinionDatamapper.modifyOpinionPosition(opinionModifiedPosition);
+        const opinionPositionModified = req.body;
+        const { result , error } = await opinionDatamapper.modifyOpinionPosition(opinionPositionModified);
         controllerUtil.manageResponse(error, result, res, next);
     },
 
