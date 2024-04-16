8ed4c83eb918b7b4b303b7899bbd2171f29d3f3e
unknown <quiniouflorent@gmail.com>
2024-04-16 13:29:33 +0200

    update for heroku connection

--- app/models/client.js
+++ app/models/client.js
@@ -6,11 +6,15 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
+    //user: process.env.PGUSER,
+    //host: process.env.PGHOST,
+    //database: process.env.PGDATABASE,
+   // password: process.env.PGPASSWORD,
+    //port: process.env.PGPORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 const client = await pool.connect();
 
